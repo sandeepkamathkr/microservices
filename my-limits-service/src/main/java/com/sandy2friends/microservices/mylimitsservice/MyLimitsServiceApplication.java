8d9bccc8bc5a1aa77310821baf1081ae3869b67c
Sandeep <sandeepkamathkr@gmail.com>
2020-04-17 09:40:02 +1000

    adding my limits service microservice

---
+++ my-limits-service/src/main/java/com/sandy2friends/microservices/mylimitsservice/MyLimitsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sandy2friends.microservices.mylimitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLimitsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLimitsServiceApplication.class, args);
+	}
+
+}
---
+++ my-limits-service/src/test/java/com/sandy2friends/microservices/mylimitsservice/MyLimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandy2friends.microservices.mylimitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
