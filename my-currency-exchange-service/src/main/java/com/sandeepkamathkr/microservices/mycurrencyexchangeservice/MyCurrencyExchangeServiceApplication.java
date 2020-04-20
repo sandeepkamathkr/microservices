0b971879502bb80453cd53b5db6bb8b861330715
Sandeep <sandeepkamathkr@gmail.com>
2020-04-20 16:07:31 +1000

    Added some more important files

---
+++ my-currency-exchange-service/src/main/java/com/sandeepkamathkr/microservices/mycurrencyexchangeservice/MyCurrencyExchangeServiceApplication.java
@@ -0,0 +1,14 @@
+package com.sandeepkamathkr.microservices.mycurrencyexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MyCurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCurrencyExchangeServiceApplication.class, args);
+	}
+
+}
