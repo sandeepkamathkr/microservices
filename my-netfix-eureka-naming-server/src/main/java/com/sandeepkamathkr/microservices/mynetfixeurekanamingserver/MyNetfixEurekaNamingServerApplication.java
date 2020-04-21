1bc9ad622198ede108b5604c27890609facf740d
Sandeep <sandeepkamathkr@gmail.com>
2020-04-21 16:07:46 +1000

    Netflix Eureka Server configuration

---
+++ my-netfix-eureka-naming-server/src/main/java/com/sandeepkamathkr/microservices/mynetfixeurekanamingserver/MyNetfixEurekaNamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.sandeepkamathkr.microservices.mynetfixeurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNetfixEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNetfixEurekaNamingServerApplication.class, args);
+	}
+
+}
---
+++ my-netfix-eureka-naming-server/src/test/java/com/sandeepkamathkr/microservices/mynetfixeurekanamingserver/MyNetfixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandeepkamathkr.microservices.mynetfixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNetfixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
